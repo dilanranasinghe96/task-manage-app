e2fd3aa7673d211717acd6fa1f577b38434bb10b
Dilan Bandara Ranasinghe <1128dilan@gmail.com>
2024-12-07 02:43:55 +0530

    updates

---
+++ task_manage_app/lib/database/db_connection.dart
@@ -0,0 +1,55 @@
+import 'dart:io' as io;
+
+import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:task_manage_app/models/task_model.dart';
+
+class DatabaseConnection {
+  static Database? _db;
+
+  Future<Database?> get db async {
+    if (_db != null) {
+      return _db;
+    }
+    _db = await initDatabase();
+    return null;
+  }
+
+  initDatabase() async {
+    io.Directory = documentDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentDirectory.path, 'task_DB');
+    var db = await openDatabase(path, version: 1, onCreate: _createDatabase);
+    return db;
+  }
+
+  _createDatabase(Database db, int version) async {
+    await db.execute(
+        "CREATE TABLE tasks(id INTEGER PRIMARY KEY AUTOINCREMENT,title TEXT NOT NULL,desc TEXT NOT NULL,dataAndTime TEXT NOT NULL)");
+  }
+
+  Future<TaskModel> insert(TaskModel taskModel) async {
+    var dbClient = await db;
+    await dbClient?.insert('tasks', taskModel.toMap());
+    return taskModel;
+  }
+
+  Future<List<TaskModel>> getTaskList() async {
+    await db;
+    final List<Map<String, Object?>> QueryResult =
+        await _db!.rawQuery("SELECT * FROM tasks");
+    return QueryResult.map((e) => TaskModel.fromMap(e)).toList();
+  }
+
+  Future<int> delete(int id) async {
+    var dbClient = await db;
+    return await dbClient!.delete('tasks', where: 'id = ?', whereArgs: [id]);
+  }
+
+  Future<int> update(TaskModel taskModel) async {
+    var dbClient = await db;
+    return await dbClient!.update('tasks', taskModel.toMap(),
+        where: 'id = ?', whereArgs: [taskModel.id]);
+  }
+}
